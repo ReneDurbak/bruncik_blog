0c00fb195440bdc308e148446a47142b2402c2c9
ReneDurbak <106143298+ReneDurbak@users.noreply.github.com>
2023-09-21 22:05:51 +0200

    singleArticlePage responsive, singleArticlePage - new close side bar method implemented on smaller resolutions, write a comment not implement and review option also not implemented yet

--- client/src/components/SingleArticlePage.jsx
+++ client/src/components/SingleArticlePage.jsx
@@ -1,14 +1,30 @@
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { useMediaQuery } from 'react-responsive'
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import commentIcon from "../assets/commentIcon.png"
 import arrowRight from "../assets/Allarticlesarrow.png"
+import arrowRightIntro from "../assets/arrowRightIntro.png"
 import twitter from "../assets/twShare.png"
 import facebook from "../assets/fbShare.png"
 import pinterest from "../assets/pintShare.png"
+import closeButton from "../assets/closebutton.png"
+
 
 export default function SingleArticlePage({articles}) {
 
+
+    //media query
+    const isLaptop = useMediaQuery({query: '(min-width: 1024px )'})
+
+
+    const [closeSideBar, setCloseSideBar] = useState(false)
+
+
+
+    
+    //Find clicked article
     const {id} = useParams()
 
     const article = articles.find((article) =>  article.id === Number(id))
@@ -17,32 +33,34 @@ export default function SingleArticlePage({articles}) {
          return article;
         })
 
+  
+    
 
 
-console.log(otherArticles)
 
 
   return (
     <>
     <Navbar/>
     {/*Main container*/}
-    <div className='2xl:max-w-[1680px] max-w-[1380px] mx-auto grid-flow-col grid-cols-2 xl:gap-x-20 md:gap-x-10 mt-[120px] grid 2xl:pl-20 lg:pl-20 sm:pl-10 pl-6'>
+    <div className={`${closeSideBar ? "2xl:max-w-[1680px] max-w-[1380px] mx-auto grid-flow-col grid-cols-2 xl:gap-x-20 md:gap-x-10 gap-x-3 mt-[120px] grid 2xl:pl-20 lg:pl-20 sm:pl-10 px-4" : "2xl:max-w-[1680px] max-w-[1380px] mx-auto grid-flow-col grid-cols-2 xl:gap-x-20 md:gap-x-10 gap-x-3 mt-[120px] grid 2xl:pl-20 lg:pl-20 sm:pl-10 pl-4"}`}>
         
 
 
         {/*Article container*/}
-        <div className="2xl:max-w-[1100px] col-span-2">
+        <div className="2xl:max-w-[1100px] col-span-2 xl:mt-0 sm:mt-[-40px] mt-[-120px]">
             <div className="col-span-2">
-            <div className="flex justify-start items-center space-x-4 mt-16 text-[13px]">
+            <div className="flex justify-start items-center space-x-4 mt-16 xl:text-[13px] sm:text-[11px] text-[9px]">
                 <div>16.AUGUST 2023, 12:45 </div>
                 <div className="text-gray-400">Reading time: 2:00</div> 
             </div>
 
-            <div className="xl:text-6xl md:text-5xl xl:my-6 md:my-4">{article.title}</div>
+            <div className="xl:text-6xl lg:text-5xl md:text-4xl  text-3xl xl:my-6 md:my-4 sm:my-3 my-4 font-bold">{article.title}</div>
 
-            <div className="flex justify-start items-center space-x-4 mt-10 xl:text-lg md:text-base">
-                <div className="text-sm">written by </div><strong className="ml-5 tracking-wider">PETER BRUNCIK</strong>
-                <div className="border-l-2 border-black pl-6 uppercase"><strong>{article.label}</strong>
+            <div className="flex justify-start items-center sm:space-x-4 space-x-2 xl:mt-10 xl:text-lg sm:text-[13px] text-[11px]">
+                <div className="xl:text-base md:text-[13px] text-[11px] font-spectral">written by </div>
+                <strong className=" tracking-wider">PETER BRUNCIK</strong>
+                <div className="border-l-2 border-black sm:pl-5 pl-2 uppercase"><strong>{article.label}</strong>
             </div>
             </div>
 
@@ -53,7 +71,7 @@ console.log(otherArticles)
 
         {/*Article content*/}
 
-        <div className="mt-16 text-base break-all ">
+        <div className="xl:mt-16 sm:mt-10 mt-7 xl:text-base sm:text-[13px] text-[14px] break-all leading-6">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula est nec sapien finibus eleifend. Nunc ut urna vitae augue ultrices eleifend ut quis tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod purus id augue aliquet fermentum. Vestibulum vulputate, erat et molestie viverra, tellus mi aliquam enim, a dictum ipsum sapien nec dolor. Nulla nec mi augue. Vestibulum at felis magna. Nam at congue tellus.
 
 Etiam id fermentum lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas ornare feugiat magna id ullamcorper. In hac habitasse platea dictumst. Nulla in urna neque. Praesent quis vestibulum dui. Donec ut dolor ut tortor laoreet maximus vel sit amet enim. Nam rutrum odio nec augue venenatis suscipit. Sed rutrum maximus dui gravida hendrerit. Sed vitae tellus ultrices, tempus tortor ac, iaculis felis. Pellentesque in turpis ipsum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc ut justo et felis dapibus placerat. Fusce faucibus tincidunt lectus quis malesuada. Maecenas tempus aliquam dui et condimentum.
@@ -69,8 +87,8 @@ Vestibulum pharetra scelerisque orci, ut scelerisque augue rutrum at. Vestibulum
         {/*Review container*/}
         <div className="mt-[125px] flex justify-center">
             <div className="text-center">
-                <h1 className="text-2xl font-bold">How do you rate this article?</h1>
-                <div>Starts</div>
+                <h1 className="lg:text-2xl md:text-xl text-[14px] font-bold">How do you rate this article?</h1>
+                <div>Stars</div>
             </div>
             
         </div>
@@ -78,26 +96,26 @@ Vestibulum pharetra scelerisque orci, ut scelerisque augue rutrum at. Vestibulum
 
         {/*Comments container*/}
         <div className="mt-[200px]">
-            <h1 className="text-4xl">Comments and reviews</h1>
+            <h1 className="xl:text-4xl md:text-2xl text-xl">Comments and reviews</h1>
 
-            <div className="flex space-x-6 uppercase mt-6">
-                <div>
+            <div className="flex space-x-6 uppercase mt-6 xl:text-lg text-xs xl:mt-8">
+                <div className="my-auto ">
                     Comments
                 </div>
 
-                <div className="flex space-x-2">
+                <div className="flex space-x-2 duration-500 ease-in-out  2xl:border-2 border-[1px] border-white   py-1 px-3 rounded-3xl lg:hover:border-black active:border-black hover:cursor-pointer ">
                     <div className="my-auto">
                         <img src={commentIcon}/>
                     </div>
 
-                    <div>
-                        Write a comments
+                    <div className="my-auto">
+                        Write a comment
                     </div>
                 </div>
             </div>
 
 
-            <div className="text-2xl mt-8 font-bold">12 thoughs on "{article.title}"</div>
+            <div className="xl:text-2xl md:text-[18px] text-base mt-8 font-bold">12 thougths on "{article.title}"</div>
 
 
             {/*Comments*/}
@@ -105,13 +123,13 @@ Vestibulum pharetra scelerisque orci, ut scelerisque augue rutrum at. Vestibulum
 
                 <div className="my-12">
                     <div>
-                        <h1 className="font-bold text-lg">Natalie B</h1>
-                        <div className="text-sm text-[#757575] mt-2">July 1, 2017 at 12:57 AM</div>
+                        <h1 className="font-bold xl:text-lg text-sm">Natalie B</h1>
+                        <div className="xl:text-sm text-[11px] text-[#757575] mt-2">July 1, 2017 at 12:57 AM</div>
                     </div>
 
-                    <div className="pl-20">
-                        <div className="border-2 border-gray-300 rounded-[10px] max-w-[480px] mt-6 p-4">
-                            <div className="text-sm">
+                    <div className="xl:pl-20 pl-8">
+                        <div className="border-2 border-gray-300 rounded-[10px] max-w-[480px] mt-6 xl:p-4 p-3">
+                            <div className="xl:text-sm text-xs">
                             Thank you for the fantastic list! I've already read 'Meditations' by Marcus Aurelius, and now I have a list of books I want to dive into next. Stoicism is an inspiring philosophical movement for me.
                             </div>
                         </div>
@@ -125,24 +143,43 @@ Vestibulum pharetra scelerisque orci, ut scelerisque augue rutrum at. Vestibulum
 
         </div>
   
+
+
+
+
         
 
     
         {/*About the author side panel*/}
-        <div className="border-l-2 mr-0 mt-[-100px]  max-h-auto z-[-1]">
-            <div className="relative xl:max-w-[400px] lg:max-w-[300px] sm:max-w-[250px] mx-auto xl:text-left text-center">
-                <div className="xl:px-20 lg:px-8 md:px-6 lg:mt-[210px] sm:mt-[180px]">
+        {closeSideBar ? 
+            
+        <div className={`${isLaptop ? "hidden" : "text-bold absolute top-20 right-8"}`} onClick={()=>setCloseSideBar(false)}>icon</div>
+    
+        :
+        
+            <div className={`${closeSideBar ? "hidden" : "relative xl:border-l-2 border-l-[1px] mr-0 sm:mt-[-100px] mt-[-125px]  max-h-auto z-[0]"}`}>
+            <div onClick={()=>setCloseSideBar(true)} className={`${isLaptop ? "hidden" : "absolute sm:left-2 sm:top-[60px] top-5"}`}>
+                <img src={closeButton} className="sm:w-[24px] w-[20px]"/>
+            </div>
+            <div className="relative xl:max-w-[400px] lg:max-w-[300px] sm:max-w-[250px] max-w-[150px] mx-auto text-left">
+                <div className="xl:pr-20 xl:pl-12 lg:px-8 md:px-6 px-4 lg:mt-[210px] md:mt-[150px] sm:mt-[180px] mt-[100px]">
 
             
-                    <h1 className="underline underline-offset-[25px] decoration-gray-300 xl:text-base lg:text-sm sm:text-xs">ABOUT THE AUTHOR</h1>
-                    <div className="mt-10 xl:text-base lg:text-sm sm:text-xs">
-                    Peter Brunčík writes about egestas dui at iaculis ultricies. Nunc pulvinar neque at tellus accumsan lobortis nec non est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam dignissim dapibus volutpat. Pellentesque iaculis sapien quam, ut fermentum enim scelerisque in. 
-                    <div className="mt-6 underline underline-offset-4  decoration-gray-300 xl:text-base lg:text-sm">Click here to learn more</div>
+                    <h1 className="underline xl:underline-offset-[25px] sm:underline-offset-[15px] underline-offset-[12px] text-[#6F6F6F] decoration-gray-300 xl:text-base lg:text-sm sm:text-xs text-[10px]">ABOUT THE AUTHOR</h1>
+                    <div className="sm:mt-10 mt-8 xl:text-base lg:text-sm sm:text-xs text-[10px]">
+                    <strong>Peter Brunčík</strong> writes about egestas dui at iaculis ultricies. Nunc pulvinar neque at tellus accumsan lobortis nec non est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam dignissim dapibus volutpat. Pellentesque iaculis sapien quam, ut fermentum enim scelerisque in. 
+                    <div className="mt-6 underline underline-offset-4  decoration-gray-300 xl:text-base lg:text-sm sm:text-[11px] text-[8px] flex space-x-1">
+                        <div className=" hover:cursor-pointer">Click here to learn more</div> 
+                        
+                        <div className="my-auto">
+                            <img src={arrowRightIntro} className="hover:scale-110 duration-100 ease-in-out xl:w-[26px] lg:w-[20px] sm:w-[18px] w-[12px]  hover:cursor-pointer"/>
+                        </div>
+                    </div>
                     </div>
 
                     {/*Add banner*/}
                     <div className="flex justify-center  my-[200px]">
-                        <div className="w-[300px] h-[400px] bg-[#D9D9D9] rounded-lg p-4 flex justify-center items-center text-xl">
+                        <div className="w-[300px] h-[400px] bg-[#D9D9D9] text-[#6F6F6F] rounded-lg p-4 flex justify-center items-center text-xl">
                             
                             <span className="rotate-[-45deg]">
                                 Miesto pre reklamu
@@ -153,19 +190,19 @@ Vestibulum pharetra scelerisque orci, ut scelerisque augue rutrum at. Vestibulum
 
                     {/*Share container*/}
                     <div className="grid justify-center text-center">
-                        <h1 className="text-xl font-bold">Share this article</h1>
+                        <h1 className="xl:text-xl sm:text-base text-xs font-bold">Share this article</h1>
                         
-                        <div className="flex space-x-4 mt-6">
-                            <div><a href>Pinterest</a></div>
-                            <div><a href>Pinterest</a></div>
-                            <div><a href>Pinterest</a></div>
+                        <div className="flex xl:space-x-8 sm:space-x-6 space-x-2 xl:mt-6 mt-4">
+                            <div><a href="https://twitter.com/peterbruncik"><img src={pinterest} className="xl:w-[58px] sm:w-[42px] w-[28px] hover:scale-110 duration-300 ease-in-out"/></a></div>
+                            <div><a href="https://twitter.com/peterbruncik"><img src={twitter} className="xl:w-[58px] sm:w-[42px] w-[28px] hover:scale-110 duration-300 ease-in-out"/></a></div>
+                            <div><a href="https://twitter.com/peterbruncik"><img src={facebook} className="xl:w-[58px] sm:w-[42px] w-[28px] hover:scale-110 duration-300 ease-in-out"/></a></div>
                         </div>
                     </div>
 
 
                     {/*Add banner*/}
                     <div className="flex justify-center  my-[200px]">
-                        <div className="w-[300px] h-[400px] bg-[#D9D9D9] rounded-lg p-4 flex justify-center items-center text-xl">
+                        <div className="w-[300px] h-[400px] bg-[#D9D9D9]  text-[#6F6F6F] rounded-lg p-4 flex justify-center items-center text-xl">
                             
                             <span className="rotate-[-45deg]">
                                 Miesto pre reklamu
@@ -176,13 +213,13 @@ Vestibulum pharetra scelerisque orci, ut scelerisque augue rutrum at. Vestibulum
 
                     {/*Read next container*/}
 
-                    <div className="text-left xl:text-base lg:text-sm sm:text-xs">
+                    <div className="text-left xl:text-base sm:text-xs text-xs">
                         <div className="divide-y-2 divide-gray-300">
-                                <h1 className="uppercase mb-4">Read next</h1>
+                                <h1 className="uppercase mb-4 text-[#6F6F6F]">Read next</h1>
                             <div className="divide-y-2 divide-gray-300 mt-2">
                             {otherArticles.map((article)=> 
                             <div>
-                                <div key={article.id} className="xl:py-6 md:py-5">
+                                <div key={article.id} className="xl:py-6 py-4">
                                     {article.title}
                                 </div>
                             </div>
@@ -190,13 +227,13 @@ Vestibulum pharetra scelerisque orci, ut scelerisque augue rutrum at. Vestibulum
                             </div>
 
 
-                            <div className="xl:pt-8 lg:pt-4 md:pt-3 pb-[250px] flex space-x-4">
-                                <div>
+                            <div className="xl:pt-8 lg:pt-4 md:pt-4 pt-4 pb-[250px] flex xl:space-x-4 md:space-x-3 space-x-2">
+                                <div className="font-bold  hover:cursor-pointer">
                                     All articles
                                 </div>
 
-                                <div>
-                                    sipka
+                                <div className="my-auto">
+                                    <img src={arrowRight} className="xl:w-[22px] w-[14px] hover:scale-105 duration-100 ease-in-out"/>
                                 </div>
                             
                             </div>
@@ -208,13 +245,17 @@ Vestibulum pharetra scelerisque orci, ut scelerisque augue rutrum at. Vestibulum
 
                 </div>
             </div>
-        </div>
+            </div>
+         
+        }
+        
+        
       
 
 
 
   
-    </div>
+        </div>
 
 
     <Footer/>

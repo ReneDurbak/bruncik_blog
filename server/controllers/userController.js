09302f02028b82fc47e5a6e7db0ec291ce21f3d5
ReneDurbak <renedurbak2@gmail.com>
2024-04-09 18:51:05 +0200

    [backend] - updated authUser and registerUser functions and implemented nodemailer

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,123 +1,169 @@
-const asyncHandler = require('express-async-handler')
-const generateToken = require('../utils/generateToken')
-const User = require('../models/userModel')
+require('dotenv').config()
+const asyncHandler = require("express-async-handler");
+const generateToken = require("../utils/generateToken");
+const User = require("../models/userModel");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_CONFIRM,
+    pass: process.env.EMAIL_PASS,
+  },
+  secure: false,
+  tls: {
+    rejectUnauthorized: false
+}
+});
+
+const generateVerificationToken = (userId) => {
+  return jwt.sign({ userId }, process.env.EMAIL_SECRET, { expiresIn: "1h" });
+};
+
+const sendVerificationEmail = (email, token) => {
+  const verificationLink = `http://localhost:5173/verify?token=${token}`;
+
+  transporter.sendMail(
+    {
+      from:  process.env.EMAIL_CONFIRM,
+      to: email,
+      subject: "Email Verification",
+      html: `
+          <p>Thank you for registering!</p>
+          <p> Click <a href="${verificationLink}">here</a> to verify your email.</p>
+        `,
+    },
+    (error, info) => {
+      if (error) {
+        console.error("Error sending verification email:", error);
+      } else {
+        console.log("Verification email sent:", info.response);
+      }
+    }
+  );
+};
 
 
 
 //route     POST users/auth
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email })
+  const user = await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))) {
-        generateToken(res, user._id)
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email
-        })
-    } else {
-        res.status(401); //401 code status - Unauthorized
-        throw new Error('Invalid email or password')
-    }
+  if(!user){
+    res.status(401); //401 code status - Unauthorized
+    throw new Error('User does not exist');
+  }
+  
+  if (!user.confirmation) {
+    res.status(401); //401 code status - Unauthorized
+    throw new Error("Please confirm your email to login");
+  }
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+
+
+    res.status(401); //401 code status - Unauthorized
+    throw new Error("Invalid email or password");
+  }
 });
-
-
-
-
 
 //route     POST users
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body
+  const { name, email, password } = req.body;
 
-    const userExists = await User.findOne({ email })
+  const userExists = await User.findOne({ email });
 
-    if (userExists) {
-        res.status(400)
-        throw new Error('User already exists')
-    }
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    const user = await User.create({
-        name,
-        email,
-        password
-    })
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-    if (user) {
-        generateToken(res, user._id)
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email
-        })
-    } else {
-        res.status(400);
-        throw new Error('Invalid user data')
-    }
+  if (user) {   
+    const token = generateVerificationToken(user._id);
+    sendVerificationEmail(email, token);
 
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
-
-
-
 
 //route     POST users/logout
 const logoutUser = asyncHandler(async (req, res) => {
-    res.cookie('jwt', '', {
-        httpOnly: true,
-        expires: new Date(0)
-    })
-    
-    res.status(200).json({ message: 'User logged out' })
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User logged out" });
 });
-
-
-
 
 //route     GET users/profile
 const getUserProfile = asyncHandler(async (req, res) => {
-    const user = {
-        _id: req.user._id,
-        name: req.user.name,
-        email: req.user.email
-    }
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
 
-    res.status(200).json(user)
+  res.status(200).json(user);
 });
-
-
-
 
 //route     PUT users/profile
 const updateUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
-    console.log(user)
-    if(user){
-        user.name = req.body.name || user.name
-        user.email = req.body.email || user.email
+  const user = await User.findById(req.user._id);
+  console.log(user);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
 
-        if(req.body.password){
-            user.password = req.body.password
-        }
-
-        const updatedUser = await user.save()
-
-        res.status(200).json({
-            _id: updatedUser._id,
-            name: updatedUser.name,
-            email: updatedUser.email
-        })
-    }else{
-        res.status(404)
-        throw new Error('User not found')
+    if (req.body.password) {
+      user.password = req.body.password;
     }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 
+
+
+
+
 module.exports = {
-    authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile
-}
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};

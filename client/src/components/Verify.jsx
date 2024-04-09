1353b4f40812863a51ac01cb272970e8e87f4a2d
ReneDurbak <renedurbak2@gmail.com>
2024-04-09 18:42:47 +0200

    [frontend] - Verify component added

---
+++ client/src/components/Verify.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const Verify = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const token = new URLSearchParams(location.search).get("token");
+
+  useEffect(() => {
+    const verifyEmail = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/verify?token=${token}`
+        );
+        setMessage(response.data.message);
+        navigate("/");
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+      }
+    };
+
+    if (token) {
+      verifyEmail();
+    }
+  }, [token]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>Email Verification, please wait...</h2>
+    </div>
+  );
+};
+
+export default Verify;

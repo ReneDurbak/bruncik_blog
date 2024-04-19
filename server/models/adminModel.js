f4cc27f8a5f57c7bbe983546c4a4571314f428b5
ReneDurbak <renedurbak2@gmail.com>
2024-04-20 00:08:21 +0200

    [backend] - added method for comparing password

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,17 +1,22 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { SHA256 } = require("crypto-js");
 
 const adminSchema = new Schema({
-    password:{
-        type: String,
-        required: true  
-    },
-    username:{
-        type: String,
-        required: true
-    },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
 
-})
+adminSchema.methods.comparePassword = async function (inputPassword) {
+  const hashedInputPassword = SHA256(inputPassword).toString();
 
+  return (await this.password) === hashedInputPassword;
+};
 
-module.exports = mongoose.model('admin', adminSchema)
+module.exports = mongoose.model("admin", adminSchema);

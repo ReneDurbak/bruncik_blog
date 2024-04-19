9d2b4cfc3e898033ebed640d2fd6bd17eea0fd64
ReneDurbak <renedurbak2@gmail.com>
2024-04-20 00:09:12 +0200

    [backend] - formatted code

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { logoutAdmin, authAdmin, getAdminProfile } = require("../controllers/adminController");
-const {protect} = require("../middleware/adminAuthMiddleware")
+const {
+  logoutAdmin,
+  authAdmin,
+  getAdminProfile,
+} = require("../controllers/adminController");
+const { protect } = require("../middleware/adminAuthMiddleware");
 
-router.post("/login", authAdmin);
+router.post("/auth", authAdmin);
 router.post("/logout", logoutAdmin);
 router.route("/profile").get(protect, getAdminProfile);
 

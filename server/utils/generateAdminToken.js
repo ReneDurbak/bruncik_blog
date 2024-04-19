6c6bb3c52c75cc48f40bb7bd1745b133d7465cd7
ReneDurbak <renedurbak2@gmail.com>
2024-04-19 16:13:10 +0200

    [backend] - created script for generating admin token

---
+++ server/utils/generateAdminToken.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const generateAdminToken = (res, adminId) => {
+    const token = jwt.sign({ adminId }, process.env.ADMIN_JWT_SECRET, {
+        expiresIn: '30d'
+    });
+
+    res.cookie('adminJwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict', // CSRF attacks prevention 
+        maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+    });
+};
+
+module.exports = generateAdminToken;

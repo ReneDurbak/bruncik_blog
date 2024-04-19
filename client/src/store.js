6f8638e4f230c800d1617a10020271eea70fab36
ReneDurbak <renedurbak2@gmail.com>
2024-04-20 00:03:59 +0200

    [backend] - implemented adminApiSlice to store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice'
 import adminAuthReducer from './slices/adminAuthSlice'
-import { apiSlice } from './slices/apiSlice';
+import {apiSlice}  from './slices/apiSlice';
+import {adminApiSlice} from './slices/adminApiSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         adminAuth: adminAuthReducer,
-        [apiSlice.reducerPath]: apiSlice.reducer
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        [adminApiSlice.reducerPath]: adminApiSlice.reducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true

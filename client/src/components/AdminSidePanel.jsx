300c48c2afd1a09c0800e86f85e293e11984e1db
ReneDurbak <renedurbak2@gmail.com>
2024-04-21 15:20:06 +0200

    [frontend] - implemented logout for admin

--- client/src/components/AdminSidePanel.jsx
+++ client/src/components/AdminSidePanel.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import { useLogoutMutation } from '../slices/admin/adminsApiSlice'
+import { adminLogout } from '../slices/admin/adminAuthSlice'
+import { useNavigate } from 'react-router-dom'
+import { IoLogOutOutline } from "react-icons/io5";
+
+
 
 
 export default function AdminSidePanel() {
+
+  //const selector = useSelector()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [adminLogoutApiCall] = useLogoutMutation()
+
+  const adminLogoutHandler = async() => {
+    try {
+      await adminLogoutApiCall()
+      console.log(adminLogoutApiCall())
+      console.log(adminLogoutApiCall().unwrap())
+      dispatch(adminLogout())
+      navigate('/admin')
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <>
     <div className="fixed left-0 top-0 h-full flex flex-col justify-between h-screen bg-[#2A6CA5] px-6 text-white w-[16rem]">
@@ -11,16 +38,15 @@ export default function AdminSidePanel() {
         <li><Link to="/admin/articles">Articles</Link></li>
         <li><Link to="/admin/push-ups">Push-ups gallery</Link></li>
         </ul>
-{/*
+
 
         <div>
-        <div className="flex justify-between">
-            <div>fotka</div>
-            <div>Peter Bruncik</div>
-            <div className="flex-end">od</div>
+        <div className="flex justify-between items-end  mb-2">
+            <div>Admin</div>
+            <IoLogOutOutline size={26} className="flex-end hover:cursor-pointer" onClick={() => adminLogoutHandler()}>od</IoLogOutOutline>
         </div>
         </div>
-  */}
+  
     </div>
       
     </>

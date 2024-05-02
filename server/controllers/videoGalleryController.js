571cb52e66a0e5a8927662665a4c68900ccfd8c0
ReneDurbak <renedurbak2@gmail.com>
2024-05-02 16:53:51 +0200

    [backend] - formatted code

--- server/controllers/videoGalleryController.js
+++ server/controllers/videoGalleryController.js
@@ -1,145 +1,132 @@
-const VideoGallery = require('../models/videoGallery')
-const fs = require('fs');
-const path = require('path');
+const VideoGallery = require("../models/videoGallery");
+const fs = require("fs");
+const path = require("path");
 
+const getAllVideoGalleries = async (req, res) => {
+  try {
+    const videoGalleries = await VideoGallery.find({});
+    res.status(200).json(videoGalleries);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: `Cannot get video galleries: ${error.message}` });
+  }
+};
 
-const getAllVideoGalleries = async(req, res) => {
-    
-    try {
-        const videoGalleries = await VideoGallery.find({})
-        res.status(200).json(videoGalleries)
+const getVideoGallery = async (req, res) => {
+  const { id } = req.params;
 
-    } catch (error) {
-        res.status(400).json({message: `Cannot get video galleries: ${error.message}`})
+  try {
+    const videoGallery = await VideoGallery.findById(id);
+    res.status(200).json(videoGallery);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: `Cannot get video gallery: ${error.message}` });
+  }
+};
+
+const createVideoGallery = async (req, res) => {
+  const { title, goal } = req.body;
+  const { filename } = req.file;
+  const imageFilename = filename;
+
+  try {
+    const postedVideoGallery = await VideoGallery.create({
+      title,
+      image: imageFilename,
+      goal,
+    });
+
+    res.status(200).json(postedVideoGallery);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: `Cannot create video gallery ${error.message}` });
+  }
+};
+
+const updateVideoGallery = async (req, res) => {
+  const { id } = req.params;
+  const { title, goal } = req.body;
+  let imageFilename;
+
+  const updateData = {};
+
+  if (title) {
+    updateData.title = title;
+  }
+  if (goal) {
+    updateData.goal = goal;
+  }
+
+  try {
+    const currentVideoGallery = await VideoGallery.findById(id);
+
+    if (!currentVideoGallery) {
+      res.status(404).json({ error: `Video  gallery not found` });
     }
-}
 
+    if (req.file) {
+      imageFilename = req.file.filename;
+      updateData.image = imageFilename;
 
-const getVideoGallery = async(req, res) => {
-    const {id} = req.params
+      if (imageFilename) {
+        updateData.image = imageFilename;
 
-    try {
-        const videoGallery = await VideoGallery.findById(id)
-        res.status(200).json(videoGallery)
-        
-    } catch (error) {
-        res.status(400).json({message: `Cannot get video gallery: ${error.message}`})
-    }
-}
-
-
-const createVideoGallery = async(req, res) => {
-    const {title, goal} = req.body
-    const {filename} = req.file
-    const imageFilename = filename
-
-
-    try {
-
-
-        const postedVideoGallery = await VideoGallery.create({
-            title,
-            image: imageFilename,
-            goal
-        })
-
-        res.status(200).json(postedVideoGallery)
-
-    } catch (error) {
-        res.status(400).json({message: `Cannot create video gallery ${error.message}`})
-    }
-}
-
-
-const updateVideoGallery = async(req, res) => {
-    const {id} = req.params
-    const {title, goal} = req.body
-    let imageFilename 
-
-    const updateData = {}
-
-    
-    if (title) {
-        updateData.title = title;
-    }
-    if (goal) {
-        updateData.goal = goal;
-    }
-
-    try {
-
-        const currentVideoGallery = await VideoGallery.findById(id)
-
-        if(!currentVideoGallery){
-            res.status(404).json({error: `Video  gallery not found`})
+        if (currentVideoGallery.image) {
+          const imagePath = path.join(
+            "public/videoGallery",
+            currentVideoGallery.image
+          );
+          fs.unlinkSync(imagePath);
         }
-
-        if(req.file){
-            imageFilename = req.file.filename
-            updateData.image = imageFilename
-            
-        if(imageFilename){
-            updateData.image = imageFilename
-
-            if(currentVideoGallery.image){
-                const imagePath = path.join('public/videoGallery', currentVideoGallery.image)
-                fs.unlinkSync(imagePath)
-            }
-        }
-
+      }
     }
 
-        const updatedVideoGallery = await VideoGallery.findOneAndUpdate(
-            {_id: id}, 
-            updateData,
-            {new: true}
-            )
+    const updatedVideoGallery = await VideoGallery.findOneAndUpdate(
+      { _id: id },
+      updateData,
+      { new: true }
+    );
 
-        res.status(200).json(updatedVideoGallery)
+    res.status(200).json(updatedVideoGallery);
+  } catch (error) {
+    res.status(400).json({ message: "Cannot update video gallery" });
+  }
+};
 
-    } catch (error) {
-        res.status(400).json({message: 'Cannot update video gallery'})
+const deleteVideoGallery = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const currentVideoGallery = await VideoGallery.findById(id);
+
+    if (!currentVideoGallery) {
+      res.status(404).json({ error: `Video  gallery not found` });
     }
-}
 
-
-
-const deleteVideoGallery = async(req, res) => {
-    const {id} = req.params
-
-    try {
-
-        const currentVideoGallery = await VideoGallery.findById(id)
-
-        if(!currentVideoGallery){
-            res.status(404).json({error: `Video  gallery not found`})
-        }
-
-
-
-            if(currentVideoGallery.image){
-                const imagePath = path.join('public/videoGallery', currentVideoGallery.image)
-                fs.unlinkSync(imagePath)
-            }
-
-        
-
-        const deletedVideoGallery = await VideoGallery.findOneAndDelete({_id: id})
-        res.status(200).json(deletedVideoGallery)
-
-    } catch (error) {
-        res.status(400).json({message: 'Cannot update video gallery'})
+    if (currentVideoGallery.image) {
+      const imagePath = path.join(
+        "public/videoGallery",
+        currentVideoGallery.image
+      );
+      fs.unlinkSync(imagePath);
     }
-}
 
-
-
+    const deletedVideoGallery = await VideoGallery.findOneAndDelete({
+      _id: id,
+    });
+    res.status(200).json(deletedVideoGallery);
+  } catch (error) {
+    res.status(400).json({ message: "Cannot update video gallery" });
+  }
+};
 
 module.exports = {
-    getAllVideoGalleries,
-    getVideoGallery,
-    createVideoGallery,
-    updateVideoGallery,
-    deleteVideoGallery
-
-}
+  getAllVideoGalleries,
+  getVideoGallery,
+  createVideoGallery,
+  updateVideoGallery,
+  deleteVideoGallery,
+};

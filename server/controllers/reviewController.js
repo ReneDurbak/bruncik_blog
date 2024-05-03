a19dcd9f589cb2c5aa2f636b298fc2e7745fd871
ReneDurbak <renedurbak2@gmail.com>
2024-05-03 15:36:46 +0200

    [backend] - added update and delete controller

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -5,55 +5,62 @@ const getAllReviews = async (req, res) => {
     const Reviews = await Review.find({}).sort({ createdAt: -1 });
     res.status(200).json(Reviews);
   } catch (error) {
-    res.status(400).json({message: `Cannot get all reviews:  ${error.message}`});
+    res
+      .status(400)
+      .json({ message: `Cannot get all reviews:  ${error.message}` });
   }
 };
 
-const postReview = async(req, res) => {
-    const {rating, labels, comment, articleId} = req.body
-    try {
-        const postReview = await Review.create({
-            rating,
-            labels,
-            comment,
-            articleId
-        })
+const postReview = async (req, res) => {
+  const { rating, labels, comment, articleId, userId, userName } = req.body;
+  try {
+    const postReview = await Review.create({
+      rating,
+      labels,
+      comment,
+      articleId,
+      userId,
+      userName
+    });
 
-        res.status(200).json(postReview)
-    } catch (error) {
-        res.status(400).json({message: `Cannot post review:  ${error.message}`});
-    }
-}
+    res.status(200).json(postReview);
+  } catch (error) {
+    res.status(400).json({ message: `Cannot post review:  ${error.message}` });
+  }
+};
 
-const updateReview = async(req, res) => {
-  const {id} = req.params
+const updateReview = async (req, res) => {
+  const { id } = req.params;
 
   try {
-    const updatedReview = await Review.findByIdAndUpdate({_id: id}, {
-      ...req.body
-    })
+    const updatedReview = await Review.findByIdAndUpdate(
+      { _id: id },
+      {
+        ...req.body,
+      }
+    );
 
-    res.status(200).json(updatedReview)
+    res.status(200).json(updatedReview);
   } catch (error) {
-    res.status(400).json({message: `Cannot update review ${error.message}`})
+    res.status(400).json({ message: `Cannot update review ${error.message}` });
   }
-}
+};
 
-const deleteReview = async(req, res) => {
-  const {id} = req.params
+const deleteReview = async (req, res) => {
+  const { id } = req.params;
 
   try {
-    const deletedReview = await Review.findByIdAndDelete({_id: id})
+    const deletedReview = await Review.findByIdAndDelete({ _id: id });
 
-    res.status(200).json(deletedReview)
+    res.status(200).json(deletedReview);
   } catch (error) {
-    res.status(400).json({message: `Cannot delete review ${error.message}`})
+    res.status(400).json({ message: `Cannot delete review ${error.message}` });
   }
-}
+};
 
 module.exports = {
-    getAllReviews,
-    postReview,
-    updateReview,
-    deleteReview
-}
+  getAllReviews,
+  postReview,
+  updateReview,
+  deleteReview,
+};

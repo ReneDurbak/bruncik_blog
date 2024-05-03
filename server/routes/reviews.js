4c5031522832438821859eb3b37716d18330a72e
ReneDurbak <renedurbak2@gmail.com>
2024-05-03 13:55:27 +0200

    [backend] - added delete and update endpoints

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const {
   getAllReviews,
-  postReview
+  postReview,
+  updateReview,
+  deleteReview
 } = require("../controllers/reviewController");
 const router = express.Router();
 
 router.get("/getAllReviews", getAllReviews);
 router.post("/postReview", postReview);
-
+router.patch("/updateReview/:id", updateReview)
+router.delete("/deleteReview/:id", deleteReview)
 
 module.exports = router

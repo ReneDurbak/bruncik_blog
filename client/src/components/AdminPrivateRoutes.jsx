5a4c1b8cbb08f1dfc5dd360d0cd6a57bbeea5d01
ReneDurbak <renedurbak2@gmail.com>
2024-06-09 21:21:26 +0200

    added helmet

--- client/src/components/AdminPrivateRoutes.jsx
+++ client/src/components/AdminPrivateRoutes.jsx
@@ -1,15 +1,21 @@
-import React, { useId } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
+import React, { useId } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
 
 export default function PrivateRoute() {
-  const {adminInfo} = useSelector((state)=> state.adminAuth)
-  return adminInfo ? <Outlet/> : <Navigate to ='/admin' replace/>
-  
-    return (
-    <div>
-      
-    </div>
-  )
+  const { adminInfo } = useSelector((state) => state.adminAuth);
+  return adminInfo ? (
+    <>
+      <Helmet>
+        <title>Admin</title>
+        <link rel="icon" type="image/svg+xml" href="/adminmode.png" />
+      </Helmet>
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to="/admin" replace />
+  );
+
+  return <div></div>;
 }

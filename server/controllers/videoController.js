96d69a25115b0998d8a4ed2a8b323510d29c941f
ReneDurbak <renedurbak2@gmail.com>
2024-06-20 20:10:45 +0200

    [backend] - reformatted code and added deleteAllVideos controller

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -1,63 +1,77 @@
-const Video = require('../models/videosModel.js');
+const Video = require("../models/videosModel.js");
 
+const getAllVideos = async (req, res) => {
+  try {
+    const videos = await Video.find({}).sort({}).populate("video_gallery");
+    res.status(200).json(videos);
+  } catch (error) {
+    res.status(400).json({ error: `Cannot fetch videos: ${error.message}` });
+  }
+};
 
-const getAllVideos = async(req, res) => {
+const getVideo = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const videos = await Video.findById(id).populate("video_gallery");
+    res.status(200).json(videos);
+  } catch (error) {
+    res.status(400).json({ error: `Cannot fetch video: ${error.message}` });
+  }
+};
+
+const createVideo = async (req, res) => {
+  const { url_link, video_gallery } = req.body;
+  try {
+    const video = await Video.create({ url_link, video_gallery });
+    res.status(200).json(video);
+  } catch (error) {
+    res.status(400).json({ error: `Cannot post video: ${error.message}` });
+  }
+};
+
+const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const video = await Video.findOneAndDelete({ _id: id });
+    res.status(200).json(video);
+  } catch (error) {
+    res.status(400).json({ error: `Cannot delete video: ${error.message}` });
+  }
+};
+
+const deleteAllVideos = async (req, res) => {
+    const { videoGalleryId } = req.body;
+  
     try {
-        const videos = await Video.find({}).sort({}).populate('video_gallery')
-        res.status(200).json(videos)
+      const result = await Video.deleteMany({ video_gallery: videoGalleryId });
+      
+      res.status(200).json({ message: "All videos in the gallery have been deleted", result });
     } catch (error) {
-        res.status(400).json({erro:`Cannot fetch videos: ${error.message}`})
+      res.status(400).json({ error: `Cannot delete all videos: ${error.message}` });
     }
-}
-
-const getVideo = async(req, res) => {
-    const {id} = req.params
-
-    try {
-        const videos = await Video.findById(id).populate('video_gallery')
-        res.status(200).json(videos)
-    } catch (error) {
-        res.status(400).json({error:`Cannot fetch video: ${error.message}`})
-    }
-}
-
-const createVideo = async(req, res) => {
-    const {url_link, video_gallery} = req.body
-    try {
-        const video = await Video.create({url_link, video_gallery})
-        res.status(200).json(video)
-    } catch (error) {
-        res.status(400).json({error: `Cannot post video: ${error.message}`})
-    }
-}
-
-const deleteVideo = async(req, res) => {
-    const {id} = req.params
-    try {
-        const video = await Video.findOneAndDelete({_id: id})
-        res.status(200).json(video)
-    } catch (error) {
-        res.status(400).json({error: `Cannot delete video: ${error.message}`})
-    }
-}
-
-const updateVideo = async(req, res) => {
-    const {id} = req.params
-    try {
-        const video = await Video.findOneAndUpdate({_id: id}, {
-            ...req.body
-        })
-        res.status(200).json(video)
-    } catch (error) {
-        res.status(400).json({error: `Cannot update video: ${error.message}`})
-    }
-}
-
+  };
+  
+const updateVideo = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const video = await Video.findOneAndUpdate(
+      { _id: id },
+      {
+        ...req.body,
+      }
+    );
+    res.status(200).json(video);
+  } catch (error) {
+    res.status(400).json({ error: `Cannot update video: ${error.message}` });
+  }
+};
 
 module.exports = {
-    getAllVideos,
-    createVideo,
-    deleteVideo,
-    updateVideo,
-    getVideo
-}
+  getAllVideos,
+  createVideo,
+  deleteVideo,
+  deleteAllVideos,
+  updateVideo,
+  getVideo,
+};

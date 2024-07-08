72b6839f29f51f40203650e44f8e193bddb28ace
ReneDurbak <renedurbak2@gmail.com>
2024-07-08 17:40:49 +0200

    [backend] - renamed videosModel -> videoModel

---
+++ server/models/videoModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const videosSchema = mongoose.Schema(
+  {
+    url_link: {
+      type: String,
+      required: true,
+    },
+    day_count: {
+      type: Number,
+      default: 1,
+    },
+    video_gallery: {
+      type: Schema.Types.ObjectId,
+      ref: "videoGallery",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+videosSchema.pre("save", async function (next) {
+  const highestDayCount = await this.constructor
+    .findOne({ video_gallery: this.video_gallery })
+    .sort("-day_count")
+    .exec();
+
+  if (highestDayCount) {
+    this.day_count = highestDayCount.day_count + 1;
+  }
+
+  next();
+});
+
+const Video = mongoose.model("Video", videosSchema);
+
+module.exports = Video;

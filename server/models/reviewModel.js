fc9ed30fefb1ae73e959614662c56805fe6c1017
ReneDurbak <renedurbak2@gmail.com>
2024-07-08 16:06:19 +0200

    [backend] - updated reviewSchema properties that are referenced to other collections

--- server/models/reviewModel.js
+++ server/models/reviewModel.js
@@ -1,35 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema({
-    rating:{
-        type: Number,
-        required: true,
-        min: 0,
-        max: 5,
-        validate: {
-            validator: (value) => Number.isInteger(value) || value % 0.5 === 0,
-            message: 'Rating must be a multiple of 0.5 or an integer between 0 and 5.'
-        }
-    },
-    labels:{
-        type: [String],
-    },
-    comment:{
-        type:String,
-    },
-    articleId:{
-        type: String,
-        required: true
-    },
-    userId:{
-        type: String,
-        required: true,
-    },
-    userName:{
-        type: String,
-        required: true,
-    },
-}, {timestamps: true})
+const Schema = mongoose.Schema;
 
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+      validate: {
+        validator: (value) => Number.isInteger(value) || value % 0.5 === 0,
+        message:
+          "Rating must be a multiple of 0.5 or an integer between 0 and 5.",
+      },
+    },
+    labels: {
+      type: [String],
+    },
+    comment: {
+      type: String,
+    },
+    articleId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "article",
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    userName: {
+      type: Schema.Types.String,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('review', reviewSchema)
+module.exports = mongoose.model("review", reviewSchema);

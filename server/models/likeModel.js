4db25b21628cda2e3cb62cc81b77e7b86c02fdec
ReneDurbak <renedurbak2@gmail.com>
2024-07-08 18:25:36 +0200

    [backend] - like model created

---
+++ server/models/likeModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const likeSchema = new mongoose.Schema(
+  {
+    videoId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Video",
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Video",
+    },
+  },
+  { timestamps: true }
+);
+
+likeSchema.index({ videoId: 1, userId: 1 }, { unique: true });
+
+module.exports = mongoose.model("like", likeSchema);

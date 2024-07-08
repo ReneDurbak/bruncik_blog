edc127a4ac9dc648007508e16d9a198bdb34cd69
ReneDurbak <renedurbak2@gmail.com>
2024-07-08 16:02:41 +0200

    [backend] - updated commentSchema properties that are referenced to other collections

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -1,25 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
-    comment:{
-        type: String,
-        required: true,
+const commentSchema = new Schema(
+  {
+    comment: {
+      type: String,
+      required: true,
     },
-    userId:{
-        type: String,
-        required: true,
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
-    userName:{
-        type: String,
-        required: true,
+    userName: {
+      type: Schema.Types.String,
+      required: true,
+      ref: "User",
     },
-    articleId:{
-        type: String,
-        required: true,
-    }
-},  { timestamps: true})
+    articleId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "article",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('comment', commentSchema)
-
+module.exports = mongoose.model("comment", commentSchema);

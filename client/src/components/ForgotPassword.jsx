ea408e3664664d2452857526d6ec5e36bca33cc1
René Durbák <renedurbak2@gmail.com>
2024-07-31 11:13:03 +0200

    [frontend] - updated buttons and text sizes

--- client/src/components/ForgotPassword.jsx
+++ client/src/components/ForgotPassword.jsx
@@ -7,7 +7,7 @@ export default function ForgotPassword() {
   return (
     <>
       <div className="font-poppins flex justify-center items-center h-screen bg-gradient-to-br from-red-100 via-yellow-200 to-purple-300 px-6">
-        <div className="flex flex-col  px-10 pt-10 pb-8 rounded-[30px] w-[40rem] outline outline-[1px] shadow-2xl">
+        <div className="flex flex-col  px-4 lg:px-10 pt-10 pb-8 rounded-[30px] w-[40rem] outline outline-[1px] shadow-2xl">
           <h1 className="text-center text-2xl md:text-3xl lg:text-4xl font-bold">
             Reset your password
           </h1>
@@ -27,18 +27,20 @@ export default function ForgotPassword() {
               </div>
             </div>
 
-            <Link to="/login">
-              <button className="float-left mt-8 md:py-2 py-1 md:px-4 px-3 text-sm lg:text-base rounded-[16px] bg-white hover:bg-slate-200 text-black hover:shadow-xl shadow-lg hover:shadow-xl outline-0 outline duration-300 ease-out">
-                Back
-              </button>
-            </Link>
+            <div className="flex justify-between items-center">
+              <Link to="/login">
+                <button className="float-left mt-8 py-2 py-1 md:px-4 px-3 text-sm lg:text-base rounded-[16px] bg-white hover:bg-slate-200 text-black hover:shadow-xl shadow-lg hover:shadow-xl outline-0 outline duration-300 ease-out">
+                  Back
+                </button>
+              </Link>
 
-            <button
-              type="submit"
-              className="float-right mt-8 md:py-2 py-1 md:px-4 px-3 text-sm lg:text-base rounded-[16px] bg-black text-white shadow-lg hover:text-slate-400 hover:shadow-xl outline-0 outline duration-300 ease-out"
-            >
-              Reset password
-            </button>
+              <button
+                type="submit"
+                className="float-right mt-8 py-2 py-1 md:px-4 px-3 text-sm lg:text-base rounded-[16px] bg-black text-white shadow-lg hover:text-slate-400 hover:shadow-xl outline-0 outline duration-300 ease-out"
+              >
+                Reset password
+              </button>
+            </div>
           </form>
         </div>
       </div>
